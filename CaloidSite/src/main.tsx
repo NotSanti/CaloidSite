8ae475db7561e76248277d3e8c070f2bae126273
SantiagoPQX <santiago@pricerqx.com>
2024-11-12 23:51:52 -0500

    fix

--- CaloidSite/src/main.tsx
+++ CaloidSite/src/main.tsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { ComingSoon, Home } from './Routes/index.ts';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ComingSoon } from "./Routes/index.ts";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <ComingSoon/>,
+    element: <ComingSoon />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
